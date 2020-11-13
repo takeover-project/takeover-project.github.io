58917e8cf228dec0c575ec7ca000ea503f04a6a1
takeover-project <63126794+takeover-project@users.noreply.github.com>
2020-11-13 13:20:18 +0300

    Update player-custom.js

--- assets/player-custom/player-custom.js
+++ assets/player-custom/player-custom.js
@@ -9,6 +9,7 @@ function playerBlock(seriesData) {
 
     var playersTypes = {
         OK: 'ok.ru',
+        Google: 'google',
         SibNet: 'sibnet',
         ProtonVideo: 'protonvideo',
         YouTube: 'youtube',
